d27f27d16b4d13c1425bbbd2dfe4038d95748f41
noumbisdenccformation <noumbissiej@gmail.com>
2025-09-28 13:23:22 +0100

    mettre image

--- src/app/face-snap/face-snap.ts
+++ src/app/face-snap/face-snap.ts
@@ -1,28 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'face-snap',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './face-snap.html',
   styleUrl: './face-snap.scss'
 })
 export class FaceSnap implements OnInit {
   title!: string;
   description!: string;
-  createdAt!:Date;
+  createdAt!: Date;
   snaps!: number;
+  imageUrl!: string;
 
   ngOnInit(): void {
-  this.title = 'Archibald';
-  this.description = 'Mon meilleur ami depuis toujours !';
-  this.createdAt = new Date();
-  this.snaps = 5;
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis toujours !';
+    this.createdAt = new Date();
+    this.snaps = 5;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
 }
-
-}
-
-  
-
-  
-
