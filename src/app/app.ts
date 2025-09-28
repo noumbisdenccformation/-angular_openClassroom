a3698dd36f418b592e5025540827b9fb06105913
noumbisdenccformation <noumbissiej@gmail.com>
2025-09-28 10:19:37 +0100

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,6 +7,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('snapface');
+export class AppComponent {
+  title = signal('Snapface');
 }
