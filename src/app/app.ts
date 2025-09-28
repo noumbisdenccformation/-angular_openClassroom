5d7ddd8135237a59e297f8ea196898e1160ba518
noumbisdenccformation <noumbissiej@gmail.com>
2025-09-28 10:25:51 +0100

    fix: correction du composant principal - ajout title signal et RouterOutlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,13 @@
-// src/app/app.ts
-
-// Mettez à jour cet import pour inclure 'signal'
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// ... autres imports
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.html',
-  // ... autres configurations ...
-  imports: [RouterOutlet, /* ... */],
+  styleUrl: './app.scss',
+  imports: [RouterOutlet]
 })
-export class App {
-    // ➡️ AJOUTEZ CETTE LIGNE :
-    title = signal('Nom de votre projet Angular');
-    
-    // ... reste de la classe ...
+export class AppComponent {
+  title = signal('Snapface');
 }
